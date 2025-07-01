32ceb058777c51fc31eba1db2c4224f397b40c06
David Garcia <dngarcia2022@gmail.com>
2025-07-01 18:19:31 -0400

    made code for smartpointer

---
+++ Pointers/smartPointers.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <memory>
+
+class Entity
+{
+public:
+    Entity()
+    {
+        std::cout << "Created Entity!" << std::endl;
+    }
+    ~Entity()
+    {
+        std::cout << "Destroyed Entity!" << std::endl;
+    }
+    void Print() {
+        std::cout << "This is a print function";
+    }
+};
+int main()
+{
+    {
+        //One way to make a unqiue pointer to an object.
+        std::unique_ptr<Entity> entity(new Entity());
+        //Prefered way to make a unique pointer
+        std::unique_ptr<Entity> entity2 = std::make_unique<Entity>(); 
+        int x = 5;
+        int* p = &x;
+    }
+
+    return 0;
+}
