6816d98f832b4deb841d8ece054ae9fd9bbfeec9
davud27 <dngarcia2022@gmail.com>
2025-07-01 23:32:31 -0400

    test

--- Library Book Manager/book.hpp
+++ Library Book Manager/book.hpp
@@ -1,4 +1,18 @@
 #ifndef BOOK_HPP
 #define BOOK_HPP
+#include <string>
+class Book
+{
+private:
+    std::string title, author;
+    int year;
+    bool isAvailable;
+
+public:
+    Book();
+    Book(std::string title, std::string author, int year, bool isAvailable);
+    ~Book();
+    void printInfo();
+};
 
 #endif
--- Library Book Manager/tempCodeRunnerFile.cpp
+++ Library Book Manager/tempCodeRunnerFile.cpp
@@ -6,6 +6,9 @@ int main(){
     Book1->printInfo();
 
 
+
+
+
+
     return 0;
 }
- 
